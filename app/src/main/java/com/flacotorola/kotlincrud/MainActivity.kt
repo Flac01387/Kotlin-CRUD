93698a9dab840a4d7571a3c6cb680e4dd5b0fe43
jose.orozco <jose.orozco@aforecoppel.com>
2022-07-10 00:01:59 -0600

    Se sube el cambio de pantalla principal a ListaActivity

--- app/src/main/java/com/flacotorola/kotlincrud/MainActivity.kt
+++ app/src/main/java/com/flacotorola/kotlincrud/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.flacotorola.kotlincrud
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Esperar 3 segundos
+        Handler(Looper.getMainLooper()).postDelayed({
+            //Cambiar de esta pantalla a ListaActivity
+            val intent = Intent(this, ListaActivity::class.java)
+            startActivity(intent)
+            //Finalizar esta pantalla
+            finish()
+        }, 3000)
     }
 }
