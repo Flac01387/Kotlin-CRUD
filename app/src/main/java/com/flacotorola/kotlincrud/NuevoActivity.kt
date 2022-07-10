afd6eaad72fe430c87bddb0713f91f401959f6fd
jose.orozco <jose.orozco@aforecoppel.com>
2022-07-10 17:54:21 -0600

    Se quitan librerias opcionales de room y se agrega libreria para corrutinas

--- app/src/main/java/com/flacotorola/kotlincrud/NuevoActivity.kt
+++ app/src/main/java/com/flacotorola/kotlincrud/NuevoActivity.kt
@@ -1,11 +1,18 @@
 package com.flacotorola.kotlincrud
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class NuevoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nuevo)
+    }
+
+    fun guardar() {
+        //Toast.makeText(this,"Click en guardar", Toast.LENGTH_SHORT).show()
     }
 }
