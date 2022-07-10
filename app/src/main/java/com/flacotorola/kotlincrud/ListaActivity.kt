6a05b3ed9169d456fc71742959b984fce778f531
jose.orozco <jose.orozco@aforecoppel.com>
2022-07-10 00:53:25 -0600

    Se agrega el cambio de pantalla con el boton nuevo

--- app/src/main/java/com/flacotorola/kotlincrud/ListaActivity.kt
+++ app/src/main/java/com/flacotorola/kotlincrud/ListaActivity.kt
@@ -1,11 +1,18 @@
 package com.flacotorola.kotlincrud
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class ListaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista)
+    }
+
+    fun nuevo(view: View) {
+        val intent = Intent(this, NuevoActivity::class.java)
+        startActivity(intent)
     }
 }
